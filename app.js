9bd56d1730a825566dfebaf4799bbb5db6cd5313
chakra2017 <bhattacharjee.chakra17@gmail.com>
2023-08-15 23:09:01 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-// alert("this script works");
-prompt("hello");
+var btn_trans = document.querySelector("#btn-translate");
+var text_input = document.querySelector("#text-input");
+var text_output = document.querySelector("#output");
+
+btn_trans.addEventListener("click", function clickeventhandler() {
+  text_output.innerText = "jajajajajaj " + text_input.value;
+});
