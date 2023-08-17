2dd0c9a2e45e6f94d2be623a231658bda287a876
chakra2017 <bhattacharjee.chakra17@gmail.com>
2023-08-17 21:35:54 +0530

    api calls and showing tanslated sentance

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ var btn_trans = document.querySelector("#btn-translate");
 var text_input = document.querySelector("#text-input");
 var text_output = document.querySelector("#output");
 
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslate(txt) {
+  return serverurl + "?" + "text=" + txt;
+}
+
+function errorhandler(error) {
+  console.log("error occured", error);
+}
+
 btn_trans.addEventListener("click", function clickeventhandler() {
-  text_output.innerText = "jajajajajaj " + text_input.value;
+  var inputtxt = text_input.value;
+  fetch(getTranslate(inputtxt))
+    .then((response) => response.json())
+    .then((json) => {
+      var ans = json.contents.translated;
+      text_output.innerText = ans;
+    })
+    .catch(errorhandler);
 });
